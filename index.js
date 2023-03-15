2392e9578cbd3b67c4eb3647a3bb7fec728d6829
Camille-BAE <camille.jbae@gmail.com>
2023-03-15 15:10:06 +0100

    clean arbo

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
-
-app.listen(port, () => {
-  console.log("Hello there ! The server is running on port " + port);
-});
