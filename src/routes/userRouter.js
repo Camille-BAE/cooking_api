ecce4ff7deb8867852562a05248e99f0ab120794
Camille-BAE <camille.jbae@gmail.com>
2023-03-15 16:02:21 +0100

    clean code

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,16 +2,6 @@ const userRouteur = require("express").Router();
 const createUser = require('../controllers/userController')
 
 // Créer un User
-// userRouteur.post("/create", async (req, res) => {
-//   try {
-//     const newUser = await new User(req.body);
-//     newUser.save();
-//     res.send(newUser);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
-
 userRouteur.post("/create", createUser);
 
 // Voir tous les Users
